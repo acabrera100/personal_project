5066e9646fabcd162b786f6d264beaf4e5b3f76f
Andres.C <andrescabrera@pursuit.org>
2019-02-20 11:50:09 -0500

    typos worst bug ever

--- tumblesBackEnd/db/queries/PostsQueries.js
+++ tumblesBackEnd/db/queries/PostsQueries.js
@@ -2,8 +2,8 @@ const {db} = require("./index.js")
 
 
 const getSinglePost = (req, res, next) => {
-  let userId = parseInt(req.params.id);
-  db.one("SELECT * FROM posts WHERE id = $1", [userId])
+  let postId = parseInt(req.params.id);
+  db.one("SELECT * FROM posts WHERE id = $1", [postId])
     .then(data => {
       res.status(200);
       res.json({
@@ -12,7 +12,9 @@ const getSinglePost = (req, res, next) => {
         body: data
       });
     })
-    .catch(err => next(err));
+    .catch(err => {
+      console.log(err)
+      return next(err)})
 };
 
 const getAllPosts = (req,res,next) =>{
@@ -25,73 +27,75 @@ const getAllPosts = (req,res,next) =>{
     body:data
   })
 })
-.catch(err => next(err));
+.catch(err => {
+      console.log(err)
+      return next(err)})
 }
 
-const getPostsByTag = ( req, res, next ) => {
- let tagId = req.params.id
- db.any('SELECT * FROM post_tags WHERE tag_id=$1',tagId)
-   .then(data => {
-     res.status(200).json({
-       status:"Success",
-       message: 'These are the posts associated with this tag',
-       body:data
-     })
-   })
-   .catch(err => {
-     res.status(404).json({
-       status:404,
-       message:'Something Went wrong! Could not GET posts by Tag id'
-     })
-    next(err)
-   })
-};
+// const getPostsByTag = ( req, res, next ) => {
+//  let tagId = req.params.id
+//  db.any('SELECT * FROM post_tags WHERE tag_id=$1',tagId)
+//    .then(data => {
+//      res.status(200).json({
+//        status:"Success",
+//        message: 'These are the posts associated with this tag',
+//        body:data
+//      })
+//    })
+//    .catch(err => {
+//      res.status(404).json({
+//        status:404,
+//        message:'Something Went wrong! Could not GET posts by Tag id'
+//      })
+//     next(err)
+//    })
+// };
 
 
 // Have not worked on the logic to determine post_type
 //  if yada yada then its a text else if video yada yada
 
-const createPost = (req, res, next) => {
-  db.none('INSERT INTO posts(text_title,text_body,url) VALUES(${text_title},${text_body},${url})', req.body)
-    .then(() => {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'You added a post to your blog!'
-        })
-    })
-    .catch(err => {
-      console.log(err)
-      return next(err)})
-}
-
-const updatePost = (req, res, next) => {
-  db.none('UPDATE posts SET text_title=${text_title}, text_body=${text_body} WHERE id=${id}',{
-    text_title: req.body.text_title,
-    text_body:req.body.text_body,
-    id: parseInt(req.params.id)
-  })
-  .then(() => {
-    res.status(200)
-      .json({
-        status: 'success',
-        message: 'Updated the post!'
-      })
-  })
-  .catch(err => {
-    console.log(err)
-    return next(err)})
-}
+// const createPost = (req, res, next) => {
+//   db.none('INSERT INTO posts(text_title,text_body,url) VALUES(${text_title},${text_body},${url})', req.body)
+//     .then(() => {
+//       res.status(200)
+//         .json({
+//           status: 'success',
+//           message: 'You added a post to your blog!'
+//         })
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       return next(err)})
+// }
+//
+// const updatePost = (req, res, next) => {
+//   db.none('UPDATE posts SET text_title=${text_title}, text_body=${text_body} WHERE id=${id}',{
+//     text_title: req.body.text_title,
+//     text_body:req.body.text_body,
+//     id: parseInt(req.params.id)
+//   })
+//   .then(() => {
+//     res.status(200)
+//       .json({
+//         status: 'success',
+//         message: 'Updated the post!'
+//       })
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     return next(err)})
+// }
 // When I am at the dashboard I want to see all the posts by me. or when I am at someone else's page I want to see all of their posts
 // I want to see the text_title, text_body, tags associated with this post
 // const getAllPostsByPost = (req,res,next) => {
-//   let userId = parseInt(req.params.id);
-//   db.one("SELECT text_title, text_body, handle,COUNT(user_id ) AS Likes FROM posts JOIN posts_tags ON post_id = posts.id JOIN tags ON tag_id = tags.id JOIN likes ON likes.post_id = posts.id WHERE posts_id = 1 GROUP BY text_title, text_body, handle", [userId])
+//   let postId = parseInt(req.params.id);
+//   db.one("SELECT text_title, text_body, handle,COUNT(post_id ) AS Likes FROM posts JOIN posts_tags ON post_id = posts.id JOIN tags ON tag_id = tags.id JOIN likes ON likes.post_id = posts.id WHERE posts_id = 1 GROUP BY text_title, text_body, handle", [postId])
 //     .then(data => {
 //       res.status(200);
 //       res.json({
 //         status: "success",
-//         message: "This is all the posts by this user",
+//         message: "This is all the posts by this post",
 //         body: data
 //       });
 //     })
@@ -99,8 +103,8 @@ const updatePost = (req, res, next) => {
 // }
 
 const deletePost = (req, res, next) => {
-  let userId = parseInt(req.params.id);
-  db.result("DELETE FROM posts WHERE id=${id}", userId)
+  let postId = parseInt(req.params.id);
+  db.result("DELETE FROM posts WHERE id=${id}", postId)
     .then(result => {
       res.status(200).json({
         status: "success",
@@ -108,8 +112,12 @@ const deletePost = (req, res, next) => {
         body: result
       });
     })
-    .catch(err => next(err));
+    .catch(err => {
+        console.log(err)
+        return next(err)})
 };
 
 
-module.exports = {createPost,updatePost,getPostsByTag,getSinglePost,getAllPosts,deletePost}
+module.exports = {getSinglePost, getAllPosts, deletePost}
+
+// createPost, updatePost,
--- tumblesBackEnd/routes/posts.js
+++ tumblesBackEnd/routes/posts.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const {createPosts,updatePosts,getPostsByTag,getSinglePosts,getAllPostss,deletePosts} = require("../db/queries/PostsQueries.js")
+const {getSinglePost,getAllPosts,deletePost} = require("../db/queries/PostsQueries.js")
 
-router.get("/",getAllPostss);
-router.get("/:id", getSinglePosts)
-router.get("/:id",getPostsByTag)
-router.post("/",createPosts)
-router.put("/:id", updatePosts)
-router.delete("/:id",deletePosts)
+router.get("/",getAllPosts);
+router.get("/:id", getSinglePost)
+// router.post("/",createPosts)
+// router.put("/:id", updatePosts)
+router.delete("/:id",deletePost)
+module.exports = router;
+
+// createPosts,updatePosts,
+
+// router.get("/:id",getPostsByTag)
+
 // router.get("/:id/posts",getAllPostsByPosts)
 // ^ I have this get at line 11 because I think I will incoporate it here. Not quite sure.
-
-module.exports = router;
