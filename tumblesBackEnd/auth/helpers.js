abf4bb90620ea31b2ef27abb8d4b50fe0645faa5
Andres.C <andrescabrera@pursuit.org>
2019-02-21 16:11:47 -0500

    helpers somewhat setup. watching reeds backend video and setting up as I go

--- tumblesBackEnd/auth/helpers.js
+++ tumblesBackEnd/auth/helpers.js
@@ -1,6 +1,30 @@
 // helpers.js is where we use bcrypt to create and verify users. We also create a piece of middleware here, loginRequired, that makes sure a session token is on our request header. We'll use this in routes that we want a user to be logged in to access.
 const pgp = require("pg-promise")({});
-const connectionString = "postgres:/localhost/tumblrdatabase"
+const connectionString = "postgres:/localhost/tumblrdatabase";
 // Line 3 not sure where it connects me just yet. ITS THE DATABASE NAME
-const db = pgp(connectionString)
-const bcrypt = require('bcryptjs');
+const db = pgp(connectionString);
+const bcrypt = require("bcryptjs");
+
+function comparePass(userPassword, databasePassword) {
+  return bcrypt.compareSync(userPassword, databasePassword);
+}
+
+function createHash(password) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+}
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    res.status(401)
+    .json({status:"Forbidden- please log in."})
+    return;
+  }
+  next();
+}
+
+module.exports ={
+  comparePass,
+  createHash,
+  loginRequired
+}
